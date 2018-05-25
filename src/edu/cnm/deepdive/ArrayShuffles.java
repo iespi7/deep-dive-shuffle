2bbb47e53753a7140ac4bf8ccc867f3d3d759888
iespi7 <ivanespinoza262@gmail.com>
2018-05-24 20:12:03 -0600

    Completed remaining updates

--- src/edu/cnm/deepdive/ArrayShuffles.java
+++ src/edu/cnm/deepdive/ArrayShuffles.java
@@ -16,6 +16,7 @@ public class ArrayShuffles {
    * 
    * @param deck data array.
    */
+
   public static void shuffle(int[] deck) {
     shuffle(deck, new Random());
   }
@@ -28,8 +29,7 @@ public class ArrayShuffles {
    * @param rng source of randomness.
    */
   
-  
-  public static void shuffle(int[] deck, Random rng) {
+    public static void shuffle(int[] deck, Random rng) {
     for(int i = deck.length - 1; i > 0; i--) {
       int source = rng.nextInt(i + 1);
       int temp = deck[i]; 
@@ -65,11 +65,26 @@ public class ArrayShuffles {
       deck[source] = temp;
     }
   }
-  
+
+  /**
+   * Shuffles deck with default source of randomness. Invokes {@link
+   * #shuffle(short[], Random)}.
+   * 
+   * @param deck data array.
+   */
+
   public static void shuffle(short[] deck) {
     shuffle(deck, new Random());
   }
   
+  /**
+   * Shuffles deck using Fisher-Yates algorithm with specified source of 
+   * randomness.
+   * 
+   * @param deck data array.
+   * @param rng source of randomness.
+   */
+
   public static void shuffle(short[] deck, Random rng) {
     for(int i = deck.length - 1; i > 0; i--) {
       int source = rng.nextInt(i + 1);
@@ -79,9 +94,24 @@ public class ArrayShuffles {
     }
   }
  
+  /**
+   * Shuffles deck with default source of randomness. Invokes {@link
+   * #shuffle(boolean[], Random)}.
+   * 
+   * @param deck data array.
+   */
+
   public static void shuffle(boolean[] deck) {
     shuffle(deck, new Random());
   }
+
+  /**
+   * Shuffles deck using Fisher-Yates algorithm with specified source of 
+   * randomness.
+   * 
+   * @param deck data array.
+   * @param rng source of randomness.
+   */
 
   public static void shuffle(boolean[] deck, Random rng) {
     for(int i = deck.length - 1; i > 0; i--) {
@@ -92,4 +122,87 @@ public class ArrayShuffles {
     }
   }
  
+  /**
+   * Shuffles deck with default source of randomness. Invokes {@link
+   * #shuffle(char[], Random)}.
+   * 
+   * @param deck data array.
+   */
+
+  public static void shuffle(char[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  /**
+   * Shuffles deck using Fisher-Yates algorithm with specified source of 
+   * randomness.
+   * 
+   * @param deck data array.
+   * @param rng source of randomness.
+   */
+
+  public static void shuffle(char[] deck, Random rng) {
+    for(int i = deck.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      char temp = deck[i]; 
+      deck[i] = deck[source];
+      deck[source] = temp;
+    }
+  }
+
+    /**
+   * Shuffles deck with default source of randomness. Invokes {@link
+   * #shuffle(float[], Random)}.
+   * 
+   * @param deck data array.
+   */
+  
+  public static void shuffle(float[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  /**
+   * Shuffles deck using Fisher-Yates algorithm with specified source of 
+   * randomness.
+   * 
+   * @param deck data array.
+   * @param rng source of randomness.
+   */
+  
+  public static void shuffle(float[] deck, Random rng) {
+    for(int i = deck.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      float temp = deck[i]; 
+      deck[i] = deck[source];
+      deck[source] = temp;
+    }
+  }
+
+  /**
+   * Shuffles deck with default source of randomness. Invokes {@link
+   * #shuffle(double[], Random)}.
+   * 
+   * @param deck data array.
+   */
+  
+  public static void shuffle(double[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  /**
+   * Shuffles deck using Fisher-Yates algorithm with specified source of 
+   * randomness.
+   * 
+   * @param deck data array.
+   * @param rng source of randomness.
+   */
+  
+  public static void shuffle(double[] deck, Random rng) {
+    for(int i = deck.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      double temp = deck[i]; 
+      deck[i] = deck[source];
+      deck[source] = temp;
+    }
+  }
 }
