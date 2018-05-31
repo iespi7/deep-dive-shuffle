39d551c492a935e1b0b8bdfc90febfb0690c2618
iespi7 <ivanespinoza262@gmail.com>
2018-05-31 14:09:12 -0600

    Replaced Stream, Reader, & Buffer with Scanner.

--- src/edu/cnm/deepdive/ArrayShufflesTest.java
+++ src/edu/cnm/deepdive/ArrayShufflesTest.java
@@ -13,37 +13,41 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ArrayShufflesTest {
   
+  private String[] names;
+  
+  @BeforeEach
+  private void readNames() throws IOException {
+    names = names("students.txt");
+  }
+  
   @Test
-  void testStringShuffle() throws IOException {
-    String[] names = names("students.txt");
-    System.out.println(Arrays.toString(names));
-    ArrayShuffles.shuffle(names);
-    System.out.println(Arrays.toString(names));
-    ArrayShuffles.shuffle(names, new SecureRandom());
-    System.out.println(Arrays.toString(names));
+  void testStringShuffle() {
+      System.out.println(Arrays.toString(names));
+      ArrayShuffles.shuffle(names);
+      System.out.println(Arrays.toString(names));
+      ArrayShuffles.shuffle(names, new SecureRandom());
+      System.out.println(Arrays.toString(names));
 
   }
   
   private String[] names(String filename) throws IOException {
-    try (
-        InputStream input = new FileInputStream(filename);
-        Reader reader = new InputStreamReader(input);
-        BufferedReader buffer = new BufferedReader(reader);
-    ) {
+    try (Scanner scanner = new Scanner (new File(filename))) {
       List<String> names = new LinkedList<>();
-      String name = buffer.readLine();
-      while (name != null) {
-      name = name.trim();
-      if (!name.isEmpty()) {
-        names.add(name);
-      }
-      name = buffer.readLine();
-    }
-    return names.toArray(new String[names.size()]);
+      while (scanner.hasNextLine()) {
+        String name = scanner.nextLine().trim();
+        if (!name.isEmpty()) {
+          names.add(name);
+        }
+       }
+      return names.toArray(new String[names.size()]);
     } 
   }
 
